5f0586d4fee12b6e96ace570f6d49aeae91f8146
Ayelen Quispe <quispeaguilar.bel3n@gmail.com>
2026-04-23 16:04:54 -0300

    Empezando con la estructura del proyecto de clima

---
+++ clima/src/componentes/navegacionEntreDias/navDias.tsx
@@ -0,0 +1,19 @@
+import { ChevronLeft, Icon } from 'lucide-react-native'
+import React from 'react'
+import { View } from 'react-native'
+
+const navEntreDias = () => {
+  return (
+    <View>
+
+        <View>
+            <Icon as= {ChevronLeft} />
+        </View>
+        <View></View>
+        <View></View>
+
+    </View>
+  )
+}
+
+export default navEntreDias
